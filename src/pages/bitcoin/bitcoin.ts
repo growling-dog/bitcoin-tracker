61ac5c04c62d8f312544f6ed88a1b1075bf3ddd6
growling-dog <42129178+growling-dog@users.noreply.github.com>
2018-08-22 23:53:19 +0800

    1st commit

---
+++ src/pages/bitcoin/bitcoin.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IonicPage} from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+
+import { BitcoinProvider } from '../../providers/bitcoin/bitcoin';
+import { Bitcoin } from '../../models/bitcoin.model'
+
+@IonicPage()
+@Component({
+  selector: 'page-bitcoin',
+  templateUrl: 'bitcoin.html',
+})
+export class BitcoinPage {
+
+  currency: string = 'GBP';
+  bitcoin$: Observable<Bitcoin>;
+
+  constructor(private bitcoinProvider: BitcoinProvider) {
+  }
+
+  ionViewWillLoad() {
+   this.getBitcoinPrice(); 
+  }
+
+  getBitcoinPrice() {
+    this.bitcoin$ = this.bitcoinProvider.getBitcoinPrice(this.currency);
+  }
+
+}
